f8e571402e47b4ee42428195e88420d43bcffd69
Dan McGuire <dgmcguire4rp@gmail.com>
2014-08-01 15:44:50 -0500

    fixes test I broke while cleaning up code

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,37 +1,37 @@
 describe 'With correct authentication credentials' do
-  let(:account) { FactoryGirl.create(:account) }
+  before(:all) do
+    @account =  FactoryGirl.create(:account)
+  end
 
   context 'signing in' do
     it 'will return a json token' do
 
       post '/sign_in',
-        session: { name: account.name, password: account.password }
+        session: { name: @account.name, password: @account.password }
 
       expect(response).to be_success
-      expect(json['token']).to eq(account.token)
+      expect(json['token']).to eq(@account.token)
     end
   end
 
   context 'signing out' do
     it 'will redirect' do
-
       get '/sign_out', nil,
         HTTP_AUTHORIZATION: \
         ActionController::HttpAuthentication::Token
-        .encode_credentials(account.token)
+        .encode_credentials(@account.token)
 
       expect(response).to have_http_status(:redirect)
     end
 
     it 'will update with a new token' do
-
+      @old = @account.token
       get '/sign_out', nil,
         HTTP_AUTHORIZATION: \
         ActionController::HttpAuthentication::Token
-        .encode_credentials(account.token)
+        .encode_credentials(@account.token)
 
-      new_token = CWB::Account.find_by_name(account.name).token
-      expect(account.token).to_not eq(new_token)
+      expect(@new).to_not eq(@old)
     end
   end
 end
