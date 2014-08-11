ac2aeadd6b780ab7ed3f994d2cc8f1433a4115df
Dan McGuire <dgmcguire4rp@gmail.com>
2014-08-11 13:45:24 -0500

    creates a more generic login/password default in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ puts 'creating five accounts'
 5.times { Fabricate(:account) }
 puts 'Creating account with know credentials...'
 puts "  name: 'dan' with password: 'password'"
-known_account = CWB::Account.new(name: 'dan', email: 'dan@test.com', password: 'password')
+known_account = CWB::Account.new(name: 'mb', email: 'mb@test.com', password: 'mb')
 known_account.save
