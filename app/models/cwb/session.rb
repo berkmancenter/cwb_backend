b492214150de85aacee1b028ac71d7f580fc697a
Ryan Clardy <ryan@metabahn.com>
2014-08-12 22:46:43 -0500

    Fix #reset_auth_token bug

--- app/models/cwb/session.rb
+++ app/models/cwb/session.rb
@@ -6,11 +6,12 @@ module CWB
     end
 
     def self.reset_auth_token(session_token)
-      account = CWB::Account.find_by_token(session_token)
-      begin
-        new_token = SecureRandom.hex
-      end while CWB::Account.exists?(token: new_token)
-      account.update_attribute(:token, new_token)
+      if account = CWB::Account.find_by_token(session_token)
+        begin
+          new_token = SecureRandom.hex
+        end while CWB::Account.exists?(token: new_token)
+        account.update_attribute(:token, new_token)
+      end
     end
 
   end
