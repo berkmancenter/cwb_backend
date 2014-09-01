6eca4507359bdb7b3578e79565aa543606d3737a
Dan McGuire <dan@metabahn.com>
2014-09-01 16:15:52 -0500

    prepends file:/
    see https://gist.github.com/hby/e52230317aea7eca258c

--- app/models/cwb/project.rb
+++ app/models/cwb/project.rb
@@ -16,39 +16,46 @@ module CWB
     end
 
     def self.project_init(params)
-      CWB::Project.create(params)
       project_dir = params[3]
       project = params[0]
 
-      Find.find(Pathname(project_dir).to_s) do |path|
-        next if path.eql? project_dir
+      if ::File.directory?(project_dir)
+        CWB::Project.create(params)
 
-        path = Pathname(path)
-        rel_path = Pathname(path.to_s[(project_dir.to_s.size+1)..-1])
-        is_toplevel = rel_path.parent.to_s.eql?('.')
+        Find.find(Pathname(project_dir).to_s) do |path|
+          next if path.eql? project_dir
 
-        uri = RDF::URI('file:/' + rel_path.to_s)
-        name = ::File.basename(path.to_s)
+          path = Pathname(path)
+          rel_path = Pathname(path.to_s[(project_dir.to_s.size+1)..-1])
+          is_toplevel = rel_path.parent.to_s.eql?('.')
 
-        next if is_toplevel && !path.directory? # we don't support files in the root directory
-        next if rel_path.basename.to_s == '.DS_Store' # ignore Mac OS X artifacts
-        next if rel_path.basename.to_s == 'Thumbs.db' # ignore Windows artifacts
+          uri = RDF::URI('file:/' + rel_path.to_s)
+          name = ::File.basename(path.to_s)
 
-        if ::File.ftype(path) == 'directory' && path.parent.to_s != '.'
-          parent = is_toplevel ? '_null' : rel_path.parent.to_s
+          next if is_toplevel && !path.directory? # we don't support files in the root directory
+          next if rel_path.basename.to_s == '.DS_Store' # ignore Mac OS X artifacts
+          next if rel_path.basename.to_s == 'Thumbs.db' # ignore Windows artifacts
 
-          params = [project,uri,name,rel_path.to_s,parent]
-          CWB::Folder.create(params)
-        elsif ::File.ftype(path) == 'file'
-          folder = 'file:/'  + ::File.basename(::File.expand_path("..", path.to_s)).to_s
-          created = ::File.ctime(path.to_s).to_datetime.to_s
-          size = ::File.size(path.to_s).to_s
-          type = FileMagic.new.file(path.to_s)
-          modified = ::File.mtime(path.to_s).to_datetime.to_s
+          if ::File.ftype(path) == 'directory' && path.parent.to_s != '.'
+            parent = is_toplevel ? '_null' : 'file:/' + rel_path.parent.to_s
 
-          params = [project,uri,name,rel_path.to_s,created,size,type,folder,modified]
-          CWB::File.create(params)
+            params = [project,uri,name,rel_path.to_s,parent]
+            CWB::Folder.create(params)
+          elsif ::File.ftype(path) == 'file'
+            folder = 'file:/'  + rel_path.to_s
+            created = ::File.ctime(path.to_s).to_datetime.to_s
+            size = ::File.size(path.to_s).to_s
+            type = FileMagic.new.file(path.to_s)
+            modified = ::File.mtime(path.to_s).to_datetime.to_s
+
+            params = [project,uri,name,rel_path.to_s,created,size,type,folder,modified]
+            CWB::File.create(params)
+          end
         end
+
+        { success: true }
+      else
+        { success: false, error: "Path must be a valid directory" }
       end
     end
   end
