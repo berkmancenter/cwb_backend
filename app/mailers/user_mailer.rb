1ae915235a212440023d367af8e248429363b0a9
Dan McGuire <dan@metabahn.com>
2014-09-29 15:08:54 -0500

    does nothing if no email provided

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,12 @@ class UserMailer < ActionMailer::Base
   default from: "dan@metabahn.com"
 
   def init_completion_email(email, success)
-    if success
-      mail(to: email, subject: 'Your project has been created successfully.')
-    else
-      mail(to: email, subject: 'Your project creation failed.')
+    if email
+      if success
+        mail(to: email, subject: 'Your project has been created successfully.')
+      else
+        mail(to: email, subject: 'Your project creation failed.')
+      end
     end
   end
 end
